4145ca20b3ed754635b735a4686ddd60a397d907
AyelmerCorzoB <corzobarbosaa@gmail.com>
2025-03-21 16:57:52 -0500

    feat: :sparkles: add domain entities for Category, Product, and Supplier with getters, setters, and toString methods

--- src/main/java/com/skeletonhexa/domain/entities/Category.java
+++ src/main/java/com/skeletonhexa/domain/entities/Category.java
@@ -1,5 +1,32 @@
 package com.skeletonhexa.domain.entities;
 
 public class Category {
+    private int id;
+    private String name;
+    private String description;
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+    @Override
+    public String toString() {
+        return "Category [id=" + id + ", name=" + name + ", description=" + description + "]";
+    }
     
 }
